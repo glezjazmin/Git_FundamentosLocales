93ff19548e447f3b51feadfebfcfa98f7f066629
glezjazmin <gonzalez08042008@gmail.com>
2025-12-04 20:25:37 -0600

    nueva interfaz

--- nucleo/urls.py
+++ nucleo/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # La ruta vacía ('') se asigna a la vista hola_mundo
-    path('', views.hola_mundo, name='hola'),
+    path('', views.inicio, name='index'),
+    
 ]
--- nucleo/views.py
+++ nucleo/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+import requests #type: ignore
 
-def hola_mundo(request):
-    """
-    Una vista simple que retorna un saludo en HTML.
-    """
-    html_content = """
-    <div style='font-family: sans-serif; text-align: center; padding-top: 50px;'>
-        <h1>¡Hola ElisaHG!</h1>
-        <p>Esta es mi primera app Django desplegada en PythonAnywhere con Github.</p>
-        <p>¡Funcionó!</p>
-    </div>
-    """
-    return HttpResponse(html_content)
+
+def inicio(request):
+    return render(request, 'inicio2.html')
